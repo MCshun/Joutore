60a36dcd426b24796cca4e807380586fe4debc3c
MCshun <shunsei0814@icloud.com>
2025-06-30 16:47:18 +0900

    Update db.js to use mysql2/promise

--- db.js
+++ db.js
@@ -1,27 +1,28 @@
 require('dotenv').config();
+const mysql = require('mysql2/promise');
 
-const mysql = require('mysql2');
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false // 自己署名証明書を許可
-  }
-});
+async function connectDB() {
+  try {
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+      ssl: { rejectUnauthorized: false }
+    });
 
+    console.log('MySQL に接続成功！');
+    return connection;
 
-connection.connect((err) => {
-  if (err) {
+  } catch (err) {
     console.error('DB接続エラー:', err);
-    return;
+    throw err;
   }
-  console.log('MySQL に接続成功！');
-});
+}
 
-module.exports = connection;
+module.exports = connectDB;
+
 
 
 
