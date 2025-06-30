1a28dba523a4abc98bbed4ba4cc194e5da5456f5
MCshun <shunsei0814@icloud.com>
2025-06-30 17:00:19 +0900

    Switch db.js to mysql2/promise createPool for promise-based queries

--- db.js
+++ db.js
@@ -1,27 +1,19 @@
 require('dotenv').config();
+
 const mysql = require('mysql2/promise');
 
-async function connectDB() {
-  try {
-    const connection = await mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      port: process.env.DB_PORT,
-      ssl: { rejectUnauthorized: false }
-    });
-
-    console.log('MySQL に接続成功！');
-    return connection;
-
-  } catch (err) {
-    console.error('DB接続エラー:', err);
-    throw err;
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false // 自己署名証明書を許可
   }
-}
+});
 
-module.exports = connectDB;
+module.exports = pool;
 
 
 
